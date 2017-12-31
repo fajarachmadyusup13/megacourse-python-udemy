9421cff671e947708220761ff86b40a45df03f3f
fajarachmadyusup13 <fajarachmady13@gmail.com>
2017-12-31 18:47:15 +0700

    updated GUI

--- S6/script1.py
+++ S6/script1.py
@@ -2,10 +2,17 @@ from tkinter import *
 
 window = Tk()
 
-b1 = Button(window, text="Execute")
+def km_to_miles():
+    print(e1_value.get())
+    miles = float(e1_value.get())*1.6
+    t1.insert(END, miles)
+
+b1 = Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0)
 
-e1 = Entry(window)
+# StringVar(), berguna untuk menampung masukan user
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value) #masukan user didapatkan melalui parameter textvariable
 e1.grid(row=0, column=1)
 
 t1 = Text(window, height=1, width=20)
